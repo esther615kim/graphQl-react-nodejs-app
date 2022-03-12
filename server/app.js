9f6957fa0019fa47ee9d9fa6173833014e3854ef
esther615kim <vilchen@naver.com>
2022-03-13 04:54:39 +0900

    add AuthorType

--- server/app.js
+++ server/app.js
@@ -2,16 +2,17 @@ const express = require('express');
 
 const { graphqlHTTP } = require('express-graphql');
 
+const schema = require('./schema/schema');
+
 const app = express();
 
 
 app.use('/graphql', graphqlHTTP({
     // add options to handle graphql requests
-    schema: schema,
-    rootValue: root,
+    schema,
+    // rootValue: root,
     graphiql: true,
   }));
-  app.
 
 
 app.listen(4000,()=>{
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,15 +1,37 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const {GraphQLObjectType,GraphQLString,GraphQLSchema} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,GraphQLInt} = graphql;
+
+//dummy
+const books =[
+    {name:"chuchu's every day", genre:"Fantasy", id:"1"},
+    {name:"chuchu's dreams", genre:"Fantasy", id:"2"},
+]
+
+const authors = [
+    {name:"Tommie C", age:34, id:"1"},
+    {name:"Mr.Chuchu", age:9, id:"2"},
+    {name:"Tommie C", age:34, id:"3"},
+]
 
 // first object type =>single book
-const Booktype = new GraphQLObjectType({
+const BookType = new GraphQLObjectType({
     name:"Book",
-    fields:()=>{
-        id:{type:GraphQLString},
-        name:{type:GraphQLString},
-        genre:{type:GraphQLString},
-    }
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name:"Author",
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type:GraphQLInt } // integer
+    })
 })
 // root query
 const RootQuery = new GraphQLObjectType({
@@ -17,14 +39,24 @@ const RootQuery = new GraphQLObjectType({
     fields:{
         //single book
         book:{
-        type:Booktype,
-        args:{id:{type:GraphQLString}},
+        type:BookType,
+        args:{id:{type:GraphQLID}},
         resolve(parent,args){ 
-            // args.id
+            // code to get data 
+            return _.find(books, { id: args.id });
         }
-        }
+        },
+        author:{
+            type:AuthorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){ 
+                // code to get data 
+                return _.find(authors, { id: args.id });
+            }
+            }
     }
 })
+
 
 module.exports = new GraphQLSchema({
     query:RootQuery // root
