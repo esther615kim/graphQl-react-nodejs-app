ac00649218c03f88192ec3221255ba05440c268d
esther615kim <vilchen@naver.com>
2022-03-17 05:58:09 +0900

    add mlab database

--- server/app.js
+++ server/app.js
@@ -4,17 +4,24 @@ const { graphqlHTTP } = require('express-graphql');
 
 const schema = require('./schema/schema');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
+// connect to mlab database
+mongoose.connect('mongodb+srv://myproject:street2133@cluster0.nwerd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log("connected to db")
+});
 
 app.use('/graphql', graphqlHTTP({
-    // add options to handle graphql requests
-    schema,
-    // rootValue: root,
-    graphiql: true,
-  }));
+  // add options to handle graphql requests
+  schema,
+  // rootValue: root,
+  graphiql: true,
+}));
 
 
-app.listen(4000,()=>{
-    console.log("listening on port 4000")
-})
+app.listen(4000, () => {
+  console.log("listening on port 4000")
+});
