f4b9aab6da959e92614239b5be5ef5c8bef49eb3
esther615kim <vilchen@naver.com>
2022-03-19 09:46:40 +0900

    add Mutation obj

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -85,7 +85,32 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        }
+    }
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery // root
-})
+    query: RootQuery, // root
+    mutation: Mutation
+});
+
+
+// module.exports = new GraphQLSchema({
+//     query: RootQuery // root
+// })
